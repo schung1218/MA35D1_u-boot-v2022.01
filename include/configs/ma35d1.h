a883867e8f963ad4ece353cb7402b2fa39f5b7c7
schung <schung@nuvoton.com>
2021-09-16 10:34:40 +0800

    Enable distro boot command and Add ma35d1_sdcard0_defconfig
    1.Enable distro boot command
    2.Add ma35d1_sdcard0_defconfig to start from  sdhci0 booting
    3.Updated MTDPARTS_DEFAULT of ma35d1_nand_defconfig and ma35d1_spinand_defconfig
    4.Set memory seze to 112MB
    
    Change-Id: Ib74bbca6c147fb55f7c7e3adcdac89dfd70a8c1c

--- include/configs/ma35d1.h
+++ include/configs/ma35d1.h
@@ -46,4 +46,113 @@
 #define STDOUT_CFG "vidconsole,serial"
 #endif
 
+#define MMCARGS \
+	"mmcboot=" \
+	"if mmc dev 0; then " \
+		"echo \"Booting form mmc ... ...\"; " \
+		"setenv bootargs root=/dev/${mmc_block} rootfstype=ext4 rw rootwait console=ttyS0,115200n8 rdinit=/sbin/init mem=${kernelmem}; " \
+		"mmc read ${kernel_addr_r} 0x1800 0x8000; " \
+		"mmc read ${fdt_addr_r} 0x1600 0x80; " \
+		"booti ${kernel_addr_r} - ${fdt_addr_r}; " \
+	"fi;\0"
+
+#define BOOTENV_DEV_LEGACY_MMC(devtypeu, devtypel, instance) \
+        "bootcmd_" #devtypel #instance "=" \
+        "run mmcboot\0"
+
+#define BOOTENV_DEV_NAME_LEGACY_MMC(devtypeu, devtypel, instance) \
+        #devtypel #instance " "
+
+#define NANDARGS \
+        "nandboot=" \
+	"if test ${mtdids} = 'nand0=nand0' ; then " \
+		"echo \"Booting from nand ... ...\"; " \
+		"setenv bootargs noinitrd ubi.mtd=${nand_ubiblock} root=ubi0:rootfs rootfstype=ubifs rw rootwait=1 console=ttyS0,115200n8 rdinit=/sbin/init mem=${kernelmem}; " \
+		"nand read ${kernel_addr_r} kernel; " \
+		"nand read ${fdt_addr_r} device-tree; " \
+		"booti ${kernel_addr_r} - ${fdt_addr_r} ; " \
+	"fi\0"
+#define BOOTENV_DEV_NAND(devtypeu, devtypel, instance) \
+	"bootcmd_" #devtypel #instance "=" \
+	"run nandboot\0"
+
+#define BOOTENV_DEV_NAME_NAND(devtypeu, devtypel, instance) \
+	#devtypel #instance " "
+
+#define SPINANDARGS \
+	"spinandboot=" \
+	"if test ${mtdids} = 'spi-nand0=spi-nand0' ; then " \
+		"echo \"Booting from spinand ... ...\"; " \
+		"mtd list; " \
+		"setenv bootargs noinitrd ubi.mtd=${spinand_ubiblock} root=ubi0:rootfs rootfstype=ubifs rw rootwait=1 console=ttyS0,115200n8 rdinit=/sbin/init mem=${kernelmem}; " \
+		"mtd read kernel ${kernel_addr_r}; " \
+		"mtd read device-tree ${fdt_addr_r}; " \
+		"booti ${kernel_addr_r} - ${fdt_addr_r}; " \
+	"fi\0"
+#define BOOTENV_DEV_SPINAND(devtypeu, devtypel, instance) \
+	"bootcmd_" #devtypel #instance "=" \
+	"run spinandboot\0"
+
+#define BOOTENV_DEV_NAME_SPINAND(devtypeu, devtypel, instance) \
+	#devtypel #instance " "
+
+
+#ifdef CONFIG_CMD_MMC
+#define BOOT_TARGET_MMC0(func)  func(MMC, mmc, 0)
+#define BOOT_TARGET_MMC1(func)  func(MMC, mmc, 1)
+#define BOOT_TARGET_LEGACY_MMC(func)  func(LEGACY_MMC, legacy_mmc, 0)
+#else
+#define BOOT_TARGET_MMC0(func)
+#define BOOT_TARGET_MMC1(func)
+#define BOOT_TARGET_LEGACY_MMC(func)
+#endif
+
+#ifdef CONFIG_CMD_UBIFS
+#define BOOT_TARGET_UBIFS(func) func(UBIFS, ubifs, 0)
+#else
+#define BOOT_TARGET_UBIFS(func)
+#endif
+
+#ifdef CONFIG_MTD_RAW_NAND
+#define BOOT_TARGET_NAND(func) func(NAND, nand, 0)
+#else
+#define BOOT_TARGET_NAND(func)
+#endif
+
+#ifdef CONFIG_CMD_MTD
+#define BOOT_TARGET_SPINAND(func) func(SPINAND, mtd, 0)
+#else
+#define BOOT_TARGET_SPINAND(func)
+#endif
+
+#define BOOT_TARGET_DEVICES(func) \
+		BOOT_TARGET_MMC0(func) \
+		BOOT_TARGET_MMC1(func) \
+		BOOT_TARGET_UBIFS(func) \
+		BOOT_TARGET_LEGACY_MMC(func) \
+		BOOT_TARGET_NAND(func) \
+		BOOT_TARGET_SPINAND(func) \
+
+#include <config_distro_bootcmd.h>
+
+/* Extra environment variables */
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootfile=Image\0" \
+	"kernel_addr_r=0x80080000\0" \
+	"fdt_addr_r=0x85000000\0" \
+	"loadaddr=0x81000000\0" \
+	"scriptaddr=0x85010000\0" \
+	"pxefile_addr_r=0x85020000\0" \
+	"ramdisk_addr_r=0x85030000\0" \
+	"bootubipart=rootfs\0" \
+	"bootubivol=rootfs\0" \
+	"kernelmem=256M\0" \
+	"mmc_block=mmcblk1p1\0" \
+	"spinand_ubiblock=9\0" \
+        "nand_ubiblock=4\0" \
+	MMCARGS \
+	NANDARGS \
+	SPINANDARGS \
+	BOOTENV
+
 #endif
